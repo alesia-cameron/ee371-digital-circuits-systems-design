b4ff88be40f46e48f855111cbd5ac190574bb3f0
alesia-cameron <aecam@uw.edu>
2025-12-30 01:22:15 -0800

    Update DE1_SoC.sv

--- lab2/DE1_SoC.sv
+++ lab2/DE1_SoC.sv
@@ -7,13 +7,12 @@ module DE1_SoC (CLOCK_50, SW, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	
 	input  logic CLOCK_50;	// 50MHz clock
 	input  logic [9:0] SW;          
-   input  logic [3:0] KEY; 
+    input  logic [3:0] KEY; 
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;	// active low
 
     //internal clock signal
     logic clk;
     assign clk = CLOCK_50;
-
 
     //========= INSTANTIATE TOP MODULE =========
 	 lab2toplevel_Part3 top_inst (
